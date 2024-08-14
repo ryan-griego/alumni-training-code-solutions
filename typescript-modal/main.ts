ecea8f1b23df22ebbccbf3266bb7ba8d2cfa7263
Ryan Griego <ryangriego@gmail.com>
2024-08-14 20:31:36 +0000

    Completed Typescript-modal exercise

---
+++ typescript-modal/main.ts
@@ -0,0 +1,21 @@
+const dismissModal = document.querySelector('.dismiss-modal');
+const openModal = document.querySelector('.open-modal');
+const firstDialog = document.querySelector('dialog');
+
+if (!dismissModal) {
+  throw new Error('No element found for .dismiss-modal');
+}
+if (!openModal) {
+  throw new Error('No element found for .open-modal');
+}
+if (!firstDialog) {
+  throw new Error('No element found for dialog');
+}
+
+openModal.addEventListener('click', function () {
+  firstDialog.showModal();
+});
+
+dismissModal.addEventListener('click', function () {
+  firstDialog.close();
+});
